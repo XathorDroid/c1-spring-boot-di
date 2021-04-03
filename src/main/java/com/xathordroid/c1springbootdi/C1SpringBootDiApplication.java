ce0582354d8972f44f450de0da8755fbd01a6b45
Xathor <andymarcel.97@gmail.com>
2021-04-03 22:08:11 +0200

    Initial commit.

---
+++ src/main/java/com/xathordroid/c1springbootdi/C1SpringBootDiApplication.java
@@ -0,0 +1,13 @@
+package com.xathordroid.c1springbootdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1SpringBootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1SpringBootDiApplication.class, args);
+	}
+
+}
